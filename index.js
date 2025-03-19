bf5e73142962bd2638aba4c24ce271901736af8d
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-03-19 11:44:24 -0300

    Feat: Add User class

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
 function addLine(dataUser){
-    var tr = document.createElement("tr");
-    tr.innerHTML = `
+    
+    console.log(dataUser);
+
+    document.getElementById('table-users').innerHTML = `
                   <tr>
                     <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
                     <td>${dataUser.name}</td>
@@ -15,9 +17,8 @@ function addLine(dataUser){
                       <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                     </td>
                   </tr>
-                  `
+                  `;
 
-                  document.getElementById('table-users').appendChild(tr);
 }
 
 document.getElementById("form-user-create").addEventListener("submit", function(event){
@@ -39,6 +40,14 @@ document.getElementById("form-user-create").addEventListener("submit", function(
     
     });
     
-    addLine(user);
+    var objectUser = new User(user.name,
+                              user.gender,
+                              user.birth,
+                              user.country,
+                              user.email,
+                              user.password,
+                              user.photo,
+                              user.admin);
+    addLine(objectUser);
 
 });
