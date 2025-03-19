dcb05cc777a5eb0efd6221d3a5a1b04e9aedc077
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-03-19 13:06:59 -0300

    Refactor: Change callback in getPhoto to use Promise

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -17,37 +17,53 @@ class UserControllers{
             event.preventDefault();
             
             let values = this.getValues();
-
-            
-            this.getPhoto((content)=>{
+            this.getPhoto().then(
+            (content)=>{
                 values.photo = content;
-
                 this.addLine(values);
 
-            });
+            },
+            (e)=>{
+                console.log(e);;
+        })
+         
+
 
         });
     };
 
-    getPhoto(callback){
+    getPhoto(){
 
-        let  fileReader = new FileReader();
 
-        let elements = [...this.formEl.elements].filter(item=>{
-            if(item.name === 'photo')
-                return item;
-            
+        return new Promise((resolve, reject)=>{
+
+            let  fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item=>{
+
+                if(item.name === 'photo')
+                    return item;
+                
+            });
+    
+            let file = elements[0].files[0];
+    
+            fileReader.onload = ()=>{
+                
+                resolve(fileReader.result);
+
+            };
+
+            fileReader.onerror = (e)=>{
+                reject(e);
+            }
+    
+            fileReader.readAsDataURL(file);
+
         });
 
-        let file = elements[0].files[0];
 
-        fileReader.onload = ()=>{
-            
-
-            callback(fileReader.result);
-        };
-
-        fileReader.readAsDataURL(file);
+       
 
     }
 
