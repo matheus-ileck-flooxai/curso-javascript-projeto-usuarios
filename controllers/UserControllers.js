66aa5e5204b9a662340ef974cb293717000e4aa6
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-03-20 10:02:11 -0300

    Feat: Add form validations in UserControllers

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -88,8 +88,20 @@ class UserControllers{
 
         let user = {};
 
+        let isValid = true;
+
         [...this.formEl.elements].forEach(function(field, index) {
     
+
+            if(['name','email','password'].indexOf(field.name) > -1 && !field.value){
+
+                field.parentElement.classList.add('has-error');
+
+                isValid = false;
+
+            }
+
+
             if(field.name == "gender"){
         
                 if(field.checked){
@@ -107,6 +119,10 @@ class UserControllers{
         
         });
         
+        if(!isValid){
+            return false;
+        }
+
         return new User(user.name,
                         user.gender,
                         user.birth,
