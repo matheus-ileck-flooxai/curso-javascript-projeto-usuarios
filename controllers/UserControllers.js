e86125101e9a550f5419246a9e049d112875b483
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-03-20 13:00:16 -0300

    Feat: Add method to remove data from LocalStorage

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -202,20 +202,10 @@ class UserControllers{
         
     }
 
-    getUsersStorage(){
-        let users = [];
-
-        if(localStorage.getItem("users")){
-
-            users = JSON.parse(localStorage.getItem("users"));
-     
-        }
-
-        return users;
-    }  
+   
     selectAll(){
        
-        let users = this.getUsersStorage();
+        let users = User.getUsersStorage();
         
         users.forEach(dataUser=>{
 
@@ -274,6 +264,12 @@ class UserControllers{
 
         tr.querySelector(".btn-delete").addEventListener('click',e=>{
             if(confirm("Deseja realmente excluir?")){
+
+                let user = new User();
+
+                user.loadFromJSON(JSON.parse(tr.dataset.user));
+
+                user.remove();
 
                 tr.remove();
 
